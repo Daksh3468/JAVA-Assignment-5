45f8bba1ecb0a3f9e445f9d53d0af5bc82b1da2b
Daksh Bangoria <96170993+Daksh3468@users.noreply.github.com>
2022-10-09 21:31:22 +0530

    Add files via upload

---
+++ pr5_4.java
@@ -0,0 +1,26 @@
+
+/*Created By 21CE007 Daksh Bangoria
+Question : WAP to show use of character and byte stream. */
+import java.io.*;
+
+class pr5_4 {
+    public static void main(String args[]) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try {
+            in = new FileInputStream("Part5Practical4Input.txt");
+            out = new FileOutputStream("Part5Practical4Output.txt");
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
